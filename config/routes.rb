38b0077624a100fa873764c019b58bd073eb0d70
themaxta <themaxta@gmail.com>
2016-05-25 01:33:11 -0600

    added users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'application#hello'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
